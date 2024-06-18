d0a68356e3e96140f259749524a9cd440989e4d9
badhaiagarwal01234 <badhaiagarwal1998@gamil.com>
2024-06-18 17:22:15 +0530

    string keywords

---
+++ string.js
@@ -0,0 +1,68 @@
+// let my="Badhai agarwal";
+
+// console.log(my[0])
+
+// console.log(my.length)
+
+// for(let i=0;i<my.length+10; i++){
+//     console.log(my[i])
+// }
+
+// console.log(typeof my)              
+
+// console.log(my.toLowerCase())
+
+// console.log(my.toUpperCase())
+
+// console.log(my.slice(2,9))           //give a words to the given index k beech k  //star from forward direction
+
+// console.log(my.slice(-9,-2))         //give a words to the given index k beech k  //star from backward direction
+
+// console.log(my.replace("a","c"))     //replace a word to the given word
+
+// console.log(my.replaceAll("a","h"))   //replace a all word "a" to the given word
+
+// console.log(my.replace("ad","kl"))   //replace a word to the given word
+
+// console.log(my.charAt(7))         //give a char of this index number
+
+// console.log(my.charCodeAt(3))    //give a ascii value
+
+// let x = "      badhai      ";
+// console.log(x.trim())            //remove the spaces of only starting and ending
+
+// let y = "      bad  hai      ";
+// console.log(y.trim())            //remove the spaces of only starting and ending, not remove the space between spelling
+
+
+// let z ="badhai";
+// let a = "singhal";
+// console.log(z.concat(a))           // adding the words
+
+// let x ="1,2,3,4,5,6,78,9";       
+// console.log (x.split(","));        //x is type of string     
+// console.log(typeof x)
+
+// let x = "1,2,3,4,5,6,7,8,9";
+// let y = x.split(",");                 //split by commas and it will be made array(object)
+// console.log(y)                        //x is type of string and then it stored in a new varible then split and then check the type of new varible ita an array(object)
+// console.log(typeof y)
+
+// let x = "1-2-3-4-5,6,7,8,9";
+//  let y = x.split("-");                 //split by commas and it will be made array(object)
+// console.log(y)                        //x is type of string and then it stored in a new varible then split and then check the type of new varible ita an array(object)
+// console.log(typeof y)
+
+
+// let a = "badhai";                 
+// console.log(a.includes("ba"))           //it is work on boolen (true/false)
+
+
+// let x = ["vivo y21T","vivo v25", "vivo zyx","samsung galaxy","oppo f12", "apple iphone"];
+
+// let vivo = x.filter((item)=>{
+//     //console.log(item)
+//         return item.includes("vivo");
+// })
+
+// console.log(vivo)
